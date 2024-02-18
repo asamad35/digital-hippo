264a83d9e1cb0ee944997785c8a6ad0213cdba42
Asamad <samad.abdus3535@gmail.com>
2024-02-18 22:08:32 +0530

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,16 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
-import MobileNav from "./MobileNav";
 import Cart from "./Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
 import { cookies } from "next/headers";
 import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
 
 const Navbar = async () => {
   const nextCookies = cookies();
   const { user } = await getServerSideUser(nextCookies);
+
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from 'http'
 import { stripeWebhookHandler } from './webhook'
 import nextBuild from 'next/dist/build'
 import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -20,7 +22,10 @@ const createContext = ({
     req,
     res,
 })
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+
+export type ExpressContext = inferAsyncReturnType<
+    typeof createContext
+>
 
 export type WebhookRequest = IncomingMessage & {
     rawBody: Buffer
@@ -43,10 +48,11 @@ const start = async () => {
         initOptions: {
             express: app,
             onInit: async (cms) => {
-                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
-            }
-        }
+                cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
     })
+
     if (process.env.NEXT_BUILD) {
         app.listen(PORT, async () => {
             payload.logger.info(
@@ -62,17 +68,40 @@ const start = async () => {
         return
     }
 
-    app.use('/api/trpc', trpcExpress.createExpressMiddleware({
-        router: appRouter,
-        createContext
-    }))
+    const cartRouter = express.Router()
+
+    cartRouter.use(payload.authenticate)
+
+    cartRouter.get('/', (req, res) => {
+        const request = req as PayloadRequest
+
+        if (!request.user)
+            return res.redirect('/sign-in?origin=cart')
+
+        const parsedUrl = parse(req.url, true)
+        const { query } = parsedUrl
+
+        return nextApp.render(req, res, '/cart', query)
+    })
+
+    app.use('/cart', cartRouter)
+    app.use(
+        '/api/trpc',
+        trpcExpress.createExpressMiddleware({
+            router: appRouter,
+            createContext,
+        })
+    )
+
     app.use((req, res) => nextHandler(req, res))
 
     nextApp.prepare().then(() => {
-        payload.logger.info(`Next.js started`)
+        payload.logger.info('Next.js started')
 
         app.listen(PORT, async () => {
-            payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+            payload.logger.info(
+                `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+            )
         })
     })
 }
